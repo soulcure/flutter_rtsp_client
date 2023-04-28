6aa777c6038c15ad9fb160d66743603abb685800
soulcure <soulcure.chen@idreamsky.com>
2023-04-29 03:49:11 +0800

    update

--- android/src/main/java/com/skyfend/rtsp/flutter_rtsp_client/FlutterRtspPlayer.java
+++ android/src/main/java/com/skyfend/rtsp/flutter_rtsp_client/FlutterRtspPlayer.java
@@ -1,29 +1,44 @@
 package com.skyfend.rtsp.flutter_rtsp_client;
 
 import android.content.Context;
-import android.view.TextureView;
+import android.net.Uri;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.List;
+
 import io.flutter.plugin.platform.PlatformView;
+
+import com.alexvas.rtsp.widget.RtspSurfaceView;
 
 class FlutterRtspPlayer implements PlatformView {
 
-    private final TextureView textureView;
+    private final RtspSurfaceView rtspSurfaceView;
 
     FlutterRtspPlayer(@NonNull Context context, int id, @Nullable Object creationParams) {
-        textureView = new TextureView(context);
+        rtspSurfaceView = new RtspSurfaceView(context);
     }
 
     @NonNull
     @Override
     public View getView() {
-        return textureView;
+        return rtspSurfaceView;
     }
 
     @Override
     public void dispose() {
     }
+
+
+    public void initialize(List<String> options) {
+        Uri uri = Uri.parse("rtsps://10.0.1.3/test.sdp");
+        String username = "admin";
+        String password = "Autel123";
+        rtspSurfaceView.init(uri, username, password);
+        rtspSurfaceView.start(true, false);
+
+    }
+
 }
